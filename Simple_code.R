c6e30dce9a7bf5c3ff84a5d238fd67214fadfef2
Esther-Kachi <estherazunna2000@gmail.com>
2026-01-22 16:19:12 +0100

    Add files via upload

---
+++ Simple_code.R
@@ -0,0 +1,10 @@
+#simple R Script
+name <- 'Esther Ginikachi Azunna'
+course_of_study <- 'Microbiology'
+Occupation <- 'Data Analyst'
+gene_x <- 'groEL'
+organism <- 'Escherichia Coli'
+
+#into a sentence
+paste0('Hello, my name is ', name, ', a ', course_of_study, ' graduate and ', Occupation, '. My favorite gene is ', gene_x, ' found in ', organism,'.')
+
